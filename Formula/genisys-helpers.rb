3fe6aedcada515b2953454bb2f77ba135016a34f
Roy van de Water <r@octoblu.com>
2017-07-27 16:04:00 -0700

    Use genisys-helpers v6.2.0

--- Formula/genisys-helpers.rb
+++ Formula/genisys-helpers.rb
@@ -1,9 +1,9 @@
 class GenisysHelpers < Formula
-  VERSION = "6.1.0"
+  VERSION = "6.2.0"
 
   desc "Helper scripts for managing project-genisys"
   url "https://github.com/octoblu/ops-tools-genisys-helpers/archive/v#{VERSION}.tar.gz"
-  sha256 "0189f68cd6bfa7f94f836d37c9dddab4eb0813bd8b8101efe06419bb10642538"
+  sha256 "a0f984714269f2e7dbb8c5509010e7be6b496aaa175fd33078bcafa5222251bf"
   depends_on 'jq'
 
   def install
@@ -19,6 +19,7 @@ class GenisysHelpers < Formula
     bin.install "genisys-find-rooms"
     bin.install "genisys-make-errors-device"
     bin.install "genisys-restart-slurries"
+    bin.install "genisys-setup-linux-citrix-receiver"
     bin.install "genisys-setup-windows-citrix-receiver"
     bin.install "genisys-update-connector-versions"
     bin.install "genisys-upsert-customer-monitor-device"
